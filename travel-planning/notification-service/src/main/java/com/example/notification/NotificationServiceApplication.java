605312c7370bfe776699d6f7b258472c574c6fee
2347259-Suraj-Biswas <suraj.biswas@mca.christuniversity.in>
2024-08-13 12:58:43 +0530

    created notification service

--- travel-planning/notification-service/src/main/java/com/example/notification/NotificationServiceApplication.java
+++ travel-planning/notification-service/src/main/java/com/example/notification/NotificationServiceApplication.java
@@ -1,13 +1,13 @@
-package com.example.booking;
+package com.example.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookingApplication {
+public class NotificationServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookingApplication.class, args);
+		SpringApplication.run(NotificationServiceApplication.class, args);
 	}
 
 }
--- travel-planning/notification-service/src/test/java/com/example/notification/NotificationServiceApplicationTests.java
+++ travel-planning/notification-service/src/test/java/com/example/notification/NotificationServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.booking;
+package com.example.notification;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BookingApplicationTests {
+class NotificationServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
